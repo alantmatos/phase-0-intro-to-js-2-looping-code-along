566396961ed405cfa80791c97c7378baa36f0680
Alan Torquatro de Matos <mr.alanmatos@hotmail.com>
2022-05-18 13:21:17 -0400

    commit tree 1c4ae8edc964d57a289ee1805ed1a185edff233c

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+// test1
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = 'surprise';
+
+
+const writeCards = function (array,event='surprise') {
+    let newArray = [];
+    
+    for ( let i=0;i<array.length;i++)
+ newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+   return newArray
+}
+
+
+
+//test 2 and 3
+const countDown = function (number) {
+    for ( let i=number;i>=0;i--)
+console.log(i)
+
+}
